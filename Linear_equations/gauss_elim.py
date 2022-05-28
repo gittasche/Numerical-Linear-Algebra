42de9a7b344678c419fa84d23111aff359b3a78d
SavkoMax <savcomax@gmail.com>
2022-05-28 18:11:51 +0300

    gauss elimination optimized

--- Linear_equations/gauss_elim.py
+++ Linear_equations/gauss_elim.py
@@ -2,18 +2,17 @@ import numpy as np
 
 def gauss_elim(A, b):
     n = len(b)
-    block = np.c_[A, b]
+    block = np.c_[A, b].astype(np.float64)
     for i in range(n - 1):
-        p = np.argmax(np.abs(A[i:, i])) + i
+        p = np.argmax(np.abs(block[i:, i])) + i
         if block[p, i] == 0:
             print('No unique solution')
             return
         
         if p != i:
-            A[[i, p]] = A[[p, i]]
+            block[[i, p]] = block[[p, i]]
         
-        for j in range(i+1, n):
-            block[j] -= block[i]*block[j, i]/block[i, i]
+        block[i+1:] -= block[i+1:, i, np.newaxis] @ block[np.newaxis, i]/block[i, i]
         
     if block[-1, -2] == 0:
         print('No unique solution')
