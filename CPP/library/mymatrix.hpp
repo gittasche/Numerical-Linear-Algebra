9bfe8f2472b4c4fdd72afad34222d687373f881f
SavkoMax <savcomax@gmail.com>
2022-06-06 01:40:05 +0300

    code formatted

--- CPP/library/mymatrix.hpp
+++ CPP/library/mymatrix.hpp
@@ -11,304 +11,346 @@ template <typename T>
 class mymatrix;
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const mymatrix<T>& vec);
+std::ostream &operator<<(std::ostream &os, const mymatrix<T> &vec);
 
 template <typename T>
-mymatrix<T> mydot(const mymatrix<T>& mat1, const mymatrix<T>& mat2);
+mymatrix<T> mydot(const mymatrix<T> &mat1, const mymatrix<T> &mat2);
 
 template <typename T>
-mymatrix<T> myouter(const mymatrix<T>& mat1, const mymatrix<T>& mat2);
+mymatrix<T> myouter(const mymatrix<T> &mat1, const mymatrix<T> &mat2);
 
 template <typename T>
-double mynorm(const mymatrix<T>& mat, const char* ord = "L2");
+double mynorm(const mymatrix<T> &mat, const char *ord = "L2");
 
 template <class T>
-class mymatrix {
+class mymatrix
+{
 private:
-	int n_, m_;
-	T** v;
+    int n_, m_;
+    T **v;
+
 public:
-	mymatrix();
-	mymatrix(int n, int m);
-	mymatrix(int n, int m, const T& a);
+    mymatrix();
+    mymatrix(int n, int m);
+    mymatrix(int n, int m, const T &a);
     mymatrix(int n, int m, const std::vector<std::vector<T>> a);
-	mymatrix(const mymatrix& mat);
+    mymatrix(const mymatrix &mat);
 
-	inline mymatrix& operator=(const mymatrix& mat);
-	inline T* operator[](const int i);
-	inline const T* operator[](const int i) const;
-	inline mymatrix operator+(const mymatrix& mat) const;
-	inline mymatrix operator-(const mymatrix& mat) const;
-	inline mymatrix& operator+=(const mymatrix& mat);
-	inline mymatrix& operator-=(const mymatrix& mat);
-	friend std::ostream& operator<<<T>(std::ostream& os, const mymatrix<T>& vec);
+    inline mymatrix &operator=(const mymatrix &mat);
+    inline T *operator[](const int i);
+    inline const T *operator[](const int i) const;
+    inline mymatrix operator+(const mymatrix &mat) const;
+    inline mymatrix operator-(const mymatrix &mat) const;
+    inline mymatrix &operator+=(const mymatrix &mat);
+    inline mymatrix &operator-=(const mymatrix &mat);
+    friend std::ostream &operator<<<T>(std::ostream &os, const mymatrix<T> &vec);
 
-	inline int nrows() const;
-	inline int ncols() const;
-	void resize(int newn, int newm);
-	void assign(int newn, int newm, const T& a);
-	friend mymatrix<T> mydot<T>(const mymatrix<T>& mat1, const mymatrix<T>& mat2);
-	friend mymatrix<T> myouter<T>(const mymatrix<T>& mat1, const mymatrix<T>& mat2);
-	friend double mynorm<T>(const mymatrix<T>& mat, const char* ord);
+    inline int nrows() const;
+    inline int ncols() const;
+    void resize(int newn, int newm);
+    void assign(int newn, int newm, const T &a);
+    friend mymatrix<T> mydot<T>(const mymatrix<T> &mat1, const mymatrix<T> &mat2);
+    friend mymatrix<T> myouter<T>(const mymatrix<T> &mat1, const mymatrix<T> &mat2);
+    friend double mynorm<T>(const mymatrix<T> &mat, const char *ord);
 
-	~mymatrix();
+    ~mymatrix();
 };
 
 template <class T>
 mymatrix<T>::mymatrix() : n_(0), m_(0), v(nullptr) {}
 
 template <class T>
-mymatrix<T>::mymatrix(int n, int m) : n_(n), m_(m), v(n > 0 ? new T*[n] : nullptr) {
-	int size = n * m;
-	if (v)
-		v[0] = size > 0 ? new T[size] : nullptr;
-	for (int i = 1; i < n; ++i)
-		v[i] = v[i - 1] + m;
+mymatrix<T>::mymatrix(int n, int m) : n_(n), m_(m), v(n > 0 ? new T *[n] : nullptr)
+{
+    int size = n * m;
+    if (v)
+        v[0] = size > 0 ? new T[size] : nullptr;
+    for (int i = 1; i < n; ++i)
+        v[i] = v[i - 1] + m;
 }
 
 template <class T>
-mymatrix<T>::mymatrix(int n, int m, const T& a) : n_(n), m_(m), v(n > 0 ? new T*[n] : nullptr) {
-	int size = n * m;
-	if (v)
-		v[0] = size > 0 ? new T[size] : nullptr;
-	for (int i = 1; i < n; ++i)
-		v[i] = v[i - 1] + m;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			v[i][j] = a;
+mymatrix<T>::mymatrix(int n, int m, const T &a) : n_(n), m_(m), v(n > 0 ? new T *[n] : nullptr)
+{
+    int size = n * m;
+    if (v)
+        v[0] = size > 0 ? new T[size] : nullptr;
+    for (int i = 1; i < n; ++i)
+        v[i] = v[i - 1] + m;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            v[i][j] = a;
 }
 
 template <class T>
-mymatrix<T>::mymatrix(int n, int m, const std::vector<std::vector<T>> a) : n_(n), m_(m), v(n > 0 ? new T*[n] : nullptr) {
-	int size = n * m;
-	if (v)
-		v[0] = size > 0 ? new T[size] : nullptr;
-	for (int i = 1; i < n; ++i)
-		v[i] = v[i - 1] + m;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			v[i][j] = a[i][j];
+mymatrix<T>::mymatrix(int n, int m, const std::vector<std::vector<T>> a) : n_(n), m_(m), v(n > 0 ? new T *[n] : nullptr)
+{
+    int size = n * m;
+    if (v)
+        v[0] = size > 0 ? new T[size] : nullptr;
+    for (int i = 1; i < n; ++i)
+        v[i] = v[i - 1] + m;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            v[i][j] = a[i][j];
 }
 
 template <class T>
-mymatrix<T>::mymatrix(const mymatrix& mat) : n_(mat.n_), m_(mat.m_), v(n_ > 0 ? new T*[n_] : nullptr) {
-	int size = n_ * m_;
-	if (v)
-		v[0] = size > 0 ? new T[size] : nullptr;
-	for (int i = 1; i < n_; ++i)
-		v[i] = v[i - 1] + m_;
-	for (int i = 0; i < n_; ++i)
-		for (int j = 0; j < m_; ++j)
-			v[i][j] = mat[i][j];
+mymatrix<T>::mymatrix(const mymatrix &mat) : n_(mat.n_), m_(mat.m_), v(n_ > 0 ? new T *[n_] : nullptr)
+{
+    int size = n_ * m_;
+    if (v)
+        v[0] = size > 0 ? new T[size] : nullptr;
+    for (int i = 1; i < n_; ++i)
+        v[i] = v[i - 1] + m_;
+    for (int i = 0; i < n_; ++i)
+        for (int j = 0; j < m_; ++j)
+            v[i][j] = mat[i][j];
 }
 
 template <class T>
-mymatrix<T>& mymatrix<T>::operator=(const mymatrix& mat) {
-	if (this != &mat) {
-		if (n_ != mat.n_ || m_ != mat.m_) {
-			if (v != nullptr) {
-				delete[] v[0];
-				delete[] v;
-			}
-			n_ = mat.n_;
-			m_ = mat.m_;
-			v = n_ > 0 ? new T*[n_] : nullptr;
-			int size = n_ * m_;
-			if (v)
-				v[0] = size > 0 ? new T[size] : nullptr;
-			for (int i = 1; i < n_; ++i)
-				v[i] = v[i - 1] + m_;
-		}
-		for (int i = 0; i < n_; ++i)
-			for (int j = 0; j < m_; ++j)
-				v[i][j] = mat[i][j];
-	}
-	return *this;
+mymatrix<T> &mymatrix<T>::operator=(const mymatrix &mat)
+{
+    if (this != &mat)
+    {
+        if (n_ != mat.n_ || m_ != mat.m_)
+        {
+            if (v != nullptr)
+            {
+                delete[] v[0];
+                delete[] v;
+            }
+            n_ = mat.n_;
+            m_ = mat.m_;
+            v = n_ > 0 ? new T *[n_] : nullptr;
+            int size = n_ * m_;
+            if (v)
+                v[0] = size > 0 ? new T[size] : nullptr;
+            for (int i = 1; i < n_; ++i)
+                v[i] = v[i - 1] + m_;
+        }
+        for (int i = 0; i < n_; ++i)
+            for (int j = 0; j < m_; ++j)
+                v[i][j] = mat[i][j];
+    }
+    return *this;
 }
 
 template <class T>
-T* mymatrix<T>::operator[](const int i) {
-	try {
-		return v[i];
-	}
-	catch (const char* s) {
-		throw("Matrix index is out of bounds.");
-	}
+T *mymatrix<T>::operator[](const int i)
+{
+    try
+    {
+        return v[i];
+    }
+    catch (const char *s)
+    {
+        throw("Matrix index is out of bounds.");
+    }
 }
 
 template <class T>
-const T* mymatrix<T>::operator[](const int i) const {
-	try {
-		return v[i];
-	}
-	catch (const char* s) {
-		throw("Matrix index is out of bounds.");
-	}
+const T *mymatrix<T>::operator[](const int i) const
+{
+    try
+    {
+        return v[i];
+    }
+    catch (const char *s)
+    {
+        throw("Matrix index is out of bounds.");
+    }
 }
 
 template <class T>
-mymatrix<T> mymatrix<T>::operator+(const mymatrix<T>& mat) const {
-	mymatrix temp(*this);
-	temp += mat;
-	return temp;
+mymatrix<T> mymatrix<T>::operator+(const mymatrix<T> &mat) const
+{
+    mymatrix temp(*this);
+    temp += mat;
+    return temp;
 }
 
 template <class T>
-mymatrix<T> mymatrix<T>::operator-(const mymatrix<T>& mat) const {
-	mymatrix temp(*this);
-	temp -= mat;
-	return temp;
+mymatrix<T> mymatrix<T>::operator-(const mymatrix<T> &mat) const
+{
+    mymatrix temp(*this);
+    temp -= mat;
+    return temp;
 }
 
 template <class T>
-mymatrix<T>& mymatrix<T>::operator+=(const mymatrix<T>& mat){
-	if (n_ != mat.n_ || m_ != mat.m_)
-		throw("Sum possible only for the matrices of same size.");
-	for (int i = 0; i < mat.n_; ++i)
-		for (int j = 0; j < mat.m_; ++j)
-			v[i][j] += mat[i][j];
-	return *this;
+mymatrix<T> &mymatrix<T>::operator+=(const mymatrix<T> &mat)
+{
+    if (n_ != mat.n_ || m_ != mat.m_)
+        throw("Sum possible only for the matrices of same size.");
+    for (int i = 0; i < mat.n_; ++i)
+        for (int j = 0; j < mat.m_; ++j)
+            v[i][j] += mat[i][j];
+    return *this;
 }
 
 template <class T>
-mymatrix<T>& mymatrix<T>::operator-=(const mymatrix<T>& mat) {
-	if (n_ != mat.n_ || m_ != mat.m_)
-		throw("Sum possible only for the matrices of same size.");
-	for (int i = 0; i < mat.n_; ++i)
-		for (int j = 0; j < mat.m_; ++j)
-			v[i][j] -= mat[i][j];
-	return *this;
+mymatrix<T> &mymatrix<T>::operator-=(const mymatrix<T> &mat)
+{
+    if (n_ != mat.n_ || m_ != mat.m_)
+        throw("Sum possible only for the matrices of same size.");
+    for (int i = 0; i < mat.n_; ++i)
+        for (int j = 0; j < mat.m_; ++j)
+            v[i][j] -= mat[i][j];
+    return *this;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const mymatrix<T>& mat) {
-	if (mat.v != nullptr) {
-		os << "[[";
-		for (int j = 0; j < mat.m_ - 1; ++j)
-			os << mat[0][j] << ", ";
-		os << mat[0][mat.m_ - 1] << "]\n";
-		for (int i = 1; i < mat.n_ - 1; ++i) {
-			os << " [";
-			for (int j = 0; j < mat.m_ - 1; ++j)
-				os << mat[i][j] << ", ";
-			os << mat[i][mat.m_ - 1] << "]\n";
-		}
-		os << " [";
-		for (int j = 0; j < mat.m_ - 1; ++j)
-			os << mat[mat.n_ - 1][j] << ", ";
-		os << mat[mat.n_ - 1][mat.m_ - 1] << "]";
-		os << "]\n";
-	}
-	else {
-		os << "Matrix undefined.\n";
-	}
-	return os;
+std::ostream &operator<<(std::ostream &os, const mymatrix<T> &mat)
+{
+    if (mat.v != nullptr)
+    {
+        os << "[[";
+        for (int j = 0; j < mat.m_ - 1; ++j)
+            os << mat[0][j] << ", ";
+        os << mat[0][mat.m_ - 1] << "]\n";
+        for (int i = 1; i < mat.n_ - 1; ++i)
+        {
+            os << " [";
+            for (int j = 0; j < mat.m_ - 1; ++j)
+                os << mat[i][j] << ", ";
+            os << mat[i][mat.m_ - 1] << "]\n";
+        }
+        os << " [";
+        for (int j = 0; j < mat.m_ - 1; ++j)
+            os << mat[mat.n_ - 1][j] << ", ";
+        os << mat[mat.n_ - 1][mat.m_ - 1] << "]";
+        os << "]\n";
+    }
+    else
+    {
+        os << "Matrix undefined.\n";
+    }
+    return os;
 }
 
 template <class T>
-int mymatrix<T>::nrows() const {
-	return n_;
+int mymatrix<T>::nrows() const
+{
+    return n_;
 }
 
 template <class T>
-int mymatrix<T>::ncols() const {
-	return m_;
+int mymatrix<T>::ncols() const
+{
+    return m_;
 }
 
 template <class T>
-void mymatrix<T>::resize(int newn, int newm) {
-	if (newn != n_ || newm != m_) {
-		if (v != nullptr) {
-			delete[] v[0];
-			delete[] v;
-		}
-		n_ = newn;
-		m_ = newm;
-		v = n_ > 0 ? new T * [n_] : nullptr;
-		int size = n_ * m_;
-		if (v)
-			v[0] = size > 0 ? new T[size] : nullptr;
-		for (int i = 1; i < n_; ++i)
-			v[i] = v[i - 1] + m_;
-	}
+void mymatrix<T>::resize(int newn, int newm)
+{
+    if (newn != n_ || newm != m_)
+    {
+        if (v != nullptr)
+        {
+            delete[] v[0];
+            delete[] v;
+        }
+        n_ = newn;
+        m_ = newm;
+        v = n_ > 0 ? new T *[n_] : nullptr;
+        int size = n_ * m_;
+        if (v)
+            v[0] = size > 0 ? new T[size] : nullptr;
+        for (int i = 1; i < n_; ++i)
+            v[i] = v[i - 1] + m_;
+    }
 }
 
 template <class T>
-void mymatrix<T>::assign(int newn, int newm, const T& a) {
-	if (newn != n_ || newm != m_) {
-		if (v != nullptr) {
-			delete[] v[0];
-			delete[] v;
-		}
-		n_ = newn;
-		m_ = newm;
-		v = n_ > 0 ? new T * [n_] : nullptr;
-		int size = n_ * m_;
-		if (v)
-			v[0] = size > 0 ? new T[size] : nullptr;
-		for (int i = 1; i < n_; ++i)
-			v[i] = v[i - 1] + m_;
-	}
-	for (int i = 0; i < n_; ++i)
-		for (int j = 0; j < m_; ++j)
-			v[i][j] = a;
+void mymatrix<T>::assign(int newn, int newm, const T &a)
+{
+    if (newn != n_ || newm != m_)
+    {
+        if (v != nullptr)
+        {
+            delete[] v[0];
+            delete[] v;
+        }
+        n_ = newn;
+        m_ = newm;
+        v = n_ > 0 ? new T *[n_] : nullptr;
+        int size = n_ * m_;
+        if (v)
+            v[0] = size > 0 ? new T[size] : nullptr;
+        for (int i = 1; i < n_; ++i)
+            v[i] = v[i - 1] + m_;
+    }
+    for (int i = 0; i < n_; ++i)
+        for (int j = 0; j < m_; ++j)
+            v[i][j] = a;
 }
 
 template <typename T>
-mymatrix<T> mydot(const mymatrix<T>& mat1, const mymatrix<T>& mat2) {
-	mymatrix<T> result(mat1.n_, mat2.m_, 0);
-	if (mat1.m_ != mat2.n_) {
-		throw("Matrix product impossible.");
-	}
-	else {
-		for (int i = 0; i < mat1.n_; ++i)
-			for (int j = 0; j < mat2.m_; ++j)
-				for (int k = 0; k < mat1.m_; ++k)
-					result[i][j] += mat1[i][k] * mat2[k][j];
-	}
-	return result;
+mymatrix<T> mydot(const mymatrix<T> &mat1, const mymatrix<T> &mat2)
+{
+    mymatrix<T> result(mat1.n_, mat2.m_, 0);
+    if (mat1.m_ != mat2.n_)
+    {
+        throw("Matrix product impossible.");
+    }
+    else
+    {
+        for (int i = 0; i < mat1.n_; ++i)
+            for (int j = 0; j < mat2.m_; ++j)
+                for (int k = 0; k < mat1.m_; ++k)
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+    }
+    return result;
 }
 
 template <typename T>
-mymatrix<T> myouter(const mymatrix<T>& mat1, const mymatrix<T>& mat2) {
-	mymatrix<T> result(mat1.n_ * mat2.n_, mat1.m_ * mat2.m_, 0);
-	for (int i = 0; i < result.n_; ++i)
-		for (int j = 0; j < result.m_; ++j)
-			result[i][j] += mat1[i / mat2.n_][j / mat2.m_] * mat2[i % mat2.n_][j % mat2.m_];
+mymatrix<T> myouter(const mymatrix<T> &mat1, const mymatrix<T> &mat2)
+{
+    mymatrix<T> result(mat1.n_ * mat2.n_, mat1.m_ * mat2.m_, 0);
+    for (int i = 0; i < result.n_; ++i)
+        for (int j = 0; j < result.m_; ++j)
+            result[i][j] += mat1[i / mat2.n_][j / mat2.m_] * mat2[i % mat2.n_][j % mat2.m_];
 
-	return result;
+    return result;
 }
 
 template <typename T>
-double mynorm(const mymatrix<T>& mat, const char* ord) {
-	if (mat.v == nullptr)
-		throw("Norm of undefined matrix does not exist.");
-	double result = 0;
-	if (ord == "L2") {
-		for (int i = 0; i < mat.n_; ++i)
-			for (int j = 0; j < mat.m_; ++j)
-				result += mat[i][j] * mat[i][j];
-	}
-	else if (ord == "L1") {
-		for (int i = 0; i < mat.n_; ++i)
-			for (int j = 0; j < mat.m_; ++j)
-				result += fabs(mat[i][j]);
-	}
-	else if (ord == "Linf") {
-		for (int i = 0; i < mat.n_; ++i)
-			for (int j = 0; j < mat.m_; ++j)
-				if (fabs(mat[i][j]) > result)
-					result = fabs(mat[i][j]);
-	}
-	return result;
+double mynorm(const mymatrix<T> &mat, const char *ord)
+{
+    if (mat.v == nullptr)
+        throw("Norm of undefined matrix does not exist.");
+    double result = 0;
+    if (ord == "L2")
+    {
+        for (int i = 0; i < mat.n_; ++i)
+            for (int j = 0; j < mat.m_; ++j)
+                result += mat[i][j] * mat[i][j];
+    }
+    else if (ord == "L1")
+    {
+        for (int i = 0; i < mat.n_; ++i)
+            for (int j = 0; j < mat.m_; ++j)
+                result += fabs(mat[i][j]);
+    }
+    else if (ord == "Linf")
+    {
+        for (int i = 0; i < mat.n_; ++i)
+            for (int j = 0; j < mat.m_; ++j)
+                if (fabs(mat[i][j]) > result)
+                    result = fabs(mat[i][j]);
+    }
+    return result;
 }
 
 template <class T>
-mymatrix<T>::~mymatrix() {
-	if (v != nullptr) {
-		delete[] v[0];
-		delete[] v;
-	}
+mymatrix<T>::~mymatrix()
+{
+    if (v != nullptr)
+    {
+        delete[] v[0];
+        delete[] v;
+    }
 }
 
 typedef std::vector<std::vector<int>> initmatint;
--- CPP/library/myvector.hpp
+++ CPP/library/myvector.hpp
@@ -12,48 +12,50 @@ template <typename T>
 class myvector;
 
 template <typename T>
-std::istream& operator>>(std::istream& is, myvector<T>& vec);
+std::istream &operator>>(std::istream &is, myvector<T> &vec);
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const myvector<T>& vec);
+std::ostream &operator<<(std::ostream &os, const myvector<T> &vec);
 
 template <typename T>
-double mydot(const myvector<T>& vec1, const myvector<T>& vec2);
+double mydot(const myvector<T> &vec1, const myvector<T> &vec2);
 
 template <typename T>
-mymatrix<T> myouter(const myvector<T>& vec1, const myvector<T>& vec2);
+mymatrix<T> myouter(const myvector<T> &vec1, const myvector<T> &vec2);
 
 template <typename T>
-double mynorm(const myvector<T>& vec, const char* ord = "L2");
+double mynorm(const myvector<T> &vec, const char *ord = "L2");
 
 template <class T>
-class myvector {
+class myvector
+{
 private:
 	int n_;
-	T* v;
+	T *v;
+
 public:
 	myvector();
 	myvector(int n);
-	myvector(int n, const T& a);
+	myvector(int n, const T &a);
 	myvector(int n, const std::vector<T> a);
-	myvector(const myvector& vec);
+	myvector(const myvector &vec);
 
-	inline myvector& operator=(const myvector& vec);
-	inline T& operator[](const int i);
-	inline const T& operator[](const int i) const;
-	inline myvector operator+(const myvector& vec) const;
-	inline myvector operator-(const myvector& vec) const;
-	inline myvector& operator+=(const myvector& vec);
-	inline myvector& operator-=(const myvector& vec);
-	friend std::istream& operator>><T>(std::istream& is, myvector<T>& vec);
-	friend std::ostream& operator<<<T>(std::ostream& os, const myvector<T>& vec);
+	inline myvector &operator=(const myvector &vec);
+	inline T &operator[](const int i);
+	inline const T &operator[](const int i) const;
+	inline myvector operator+(const myvector &vec) const;
+	inline myvector operator-(const myvector &vec) const;
+	inline myvector &operator+=(const myvector &vec);
+	inline myvector &operator-=(const myvector &vec);
+	friend std::istream &operator>><T>(std::istream &is, myvector<T> &vec);
+	friend std::ostream &operator<<<T>(std::ostream &os, const myvector<T> &vec);
 
 	inline int size() const;
 	void resize(int newn);
-	void assign(int newn, const T& a);
-	friend double mydot<T>(const myvector<T>& vec1, const myvector<T>& vec2);
-	friend mymatrix<T> myouter<T>(const myvector<T>& vec1, const myvector<T>& vec2);
-	friend double mynorm<T>(const myvector<T>& vec, const char* ord);
+	void assign(int newn, const T &a);
+	friend double mydot<T>(const myvector<T> &vec1, const myvector<T> &vec2);
+	friend mymatrix<T> myouter<T>(const myvector<T> &vec1, const myvector<T> &vec2);
+	friend double mynorm<T>(const myvector<T> &vec, const char *ord);
 
 	~myvector();
 };
@@ -65,27 +67,33 @@ template <class T>
 myvector<T>::myvector(int n) : n_(n), v(n > 0 ? new T[n] : nullptr) {}
 
 template <class T>
-myvector<T>::myvector(int n, const T& a) : n_(n), v(n > 0 ? new T[n] : nullptr) {
+myvector<T>::myvector(int n, const T &a) : n_(n), v(n > 0 ? new T[n] : nullptr)
+{
 	for (int i = 0; i < n; ++i)
 		v[i] = a;
 }
 
 template <class T>
-myvector<T>::myvector(int n, const std::vector<T> a) : n_(n), v(n > 0 ? new T[n] : nullptr) {
+myvector<T>::myvector(int n, const std::vector<T> a) : n_(n), v(n > 0 ? new T[n] : nullptr)
+{
 	for (int i = 0; i < n; ++i)
 		v[i] = a[i];
 }
 
 template <class T>
-myvector<T>::myvector(const myvector<T>& vec) : n_(vec.n_), v(n_ > 0 ? new T[n_] : nullptr) {
+myvector<T>::myvector(const myvector<T> &vec) : n_(vec.n_), v(n_ > 0 ? new T[n_] : nullptr)
+{
 	for (int i = 0; i < n_; ++i)
 		v[i] = vec[i];
 }
 
 template <class T>
-myvector<T>& myvector<T>::operator=(const myvector& vec) {
-	if (this != &vec) {
-		if (n_ != vec.n_) {
+myvector<T> &myvector<T>::operator=(const myvector &vec)
+{
+	if (this != &vec)
+	{
+		if (n_ != vec.n_)
+		{
 			if (v != nullptr)
 				delete[] v;
 			n_ = vec.n_;
@@ -98,41 +106,50 @@ myvector<T>& myvector<T>::operator=(const myvector& vec) {
 }
 
 template <class T>
-T& myvector<T>::operator[](const int i) {
-	try {
+T &myvector<T>::operator[](const int i)
+{
+	try
+	{
 		return v[i];
 	}
-	catch (const char* s) {
+	catch (const char *s)
+	{
 		throw("Vector index is out of bounds.");
 	}
 }
 
 template <class T>
-const T& myvector<T>::operator[](const int i) const {
-	try {
+const T &myvector<T>::operator[](const int i) const
+{
+	try
+	{
 		return v[i];
 	}
-	catch (const char* s) {
+	catch (const char *s)
+	{
 		throw("Vector index is out of bounds.");
 	}
 }
 
 template <class T>
-myvector<T> myvector<T>::operator+(const myvector& vec) const {
+myvector<T> myvector<T>::operator+(const myvector &vec) const
+{
 	myvector temp(*this);
 	temp += vec;
 	return temp;
 }
 
 template <class T>
-myvector<T> myvector<T>::operator-(const myvector& vec) const {
+myvector<T> myvector<T>::operator-(const myvector &vec) const
+{
 	myvector temp(*this);
 	temp += vec;
 	return temp;
 }
 
 template <class T>
-myvector<T>& myvector<T>::operator+=(const myvector& vec) {
+myvector<T> &myvector<T>::operator+=(const myvector &vec)
+{
 	if (n_ != vec.n_)
 		throw("Sum possible only for the vectors of same size.");
 	for (int i = 0; i < n_; ++i)
@@ -141,7 +158,8 @@ myvector<T>& myvector<T>::operator+=(const myvector& vec) {
 }
 
 template <class T>
-myvector<T>& myvector<T>::operator-=(const myvector& vec) {
+myvector<T> &myvector<T>::operator-=(const myvector &vec)
+{
 	if (n_ != vec.n_)
 		throw("Sum possible only for the vectors of same size.");
 	for (int i = 0; i < n_; ++i)
@@ -150,39 +168,48 @@ myvector<T>& myvector<T>::operator-=(const myvector& vec) {
 }
 
 template <typename T>
-std::istream& operator>>(std::istream& is, myvector<T>& vec) {
-	if (vec.v != nullptr) {
+std::istream &operator>>(std::istream &is, myvector<T> &vec)
+{
+	if (vec.v != nullptr)
+	{
 		for (int i = 0; i < vec.n_; ++i)
 			is >> vec[i];
 	}
-	else {
+	else
+	{
 		throw("Vector is nullptr.");
 	}
 	return is;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const myvector<T>& vec) {
-	if (vec.v != nullptr) {
+std::ostream &operator<<(std::ostream &os, const myvector<T> &vec)
+{
+	if (vec.v != nullptr)
+	{
 		os << "[";
 		for (int i = 0; i < vec.n_ - 1; ++i)
 			os << vec[i] << ", ";
 		os << vec[vec.n_ - 1] << "]\n";
 	}
-	else {
+	else
+	{
 		os << "Vector undefined.\n";
 	}
 	return os;
 }
 
 template <class T>
-int myvector<T>::size() const {
+int myvector<T>::size() const
+{
 	return n_;
 }
 
 template <class T>
-void myvector<T>::resize(int newn) {
-	if (newn != n_) {
+void myvector<T>::resize(int newn)
+{
+	if (newn != n_)
+	{
 		if (v != nullptr)
 			delete[] v;
 		n_ = newn;
@@ -191,8 +218,10 @@ void myvector<T>::resize(int newn) {
 }
 
 template <class T>
-void myvector<T>::assign(int newn, const T& a) {
-	if (newn != n_) {
+void myvector<T>::assign(int newn, const T &a)
+{
+	if (newn != n_)
+	{
 		if (v != nullptr)
 			delete[] v;
 		n_ = newn;
@@ -203,11 +232,13 @@ void myvector<T>::assign(int newn, const T& a) {
 }
 
 template <typename T>
-double mydot(const myvector<T>& vec1, const myvector<T>& vec2) {
+double mydot(const myvector<T> &vec1, const myvector<T> &vec2)
+{
 	double result = 0;
 	if (vec1.n_ != vec2.n_)
 		throw("Dot product possible only for vectors of the same size.");
-	else {
+	else
+	{
 		for (int i = 0; i < vec1.n_; ++i)
 			result += vec1[i] * vec2[i];
 	}
@@ -215,7 +246,8 @@ double mydot(const myvector<T>& vec1, const myvector<T>& vec2) {
 }
 
 template <typename T>
-mymatrix<T> myouter(const myvector<T>& vec1, const myvector<T>& vec2) {
+mymatrix<T> myouter(const myvector<T> &vec1, const myvector<T> &vec2)
+{
 	mymatrix<T> mat(vec1.n_, vec2.n_);
 	for (int i = 0; i < vec1.n_; ++i)
 		for (int j = 0; j < vec2.n_; ++j)
@@ -224,33 +256,39 @@ mymatrix<T> myouter(const myvector<T>& vec1, const myvector<T>& vec2) {
 }
 
 template <typename T>
-double mynorm(const myvector<T>& vec, const char* ord) {
+double mynorm(const myvector<T> &vec, const char *ord)
+{
 	if (vec.v == nullptr)
 		throw("Norm of undefined vector does not exist.");
 	double result = 0;
-	if (ord == "L2") {
+	if (ord == "L2")
+	{
 		for (int i = 0; i < vec.n_; ++i)
 			result += vec[i] * vec[i];
 		return sqrt(result);
 	}
-	else if (ord == "L1") {
+	else if (ord == "L1")
+	{
 		for (int i = 0; i < vec.n_; ++i)
 			result += fabs(vec[i]);
 		return result;
 	}
-	else if (ord == "Linf") {
+	else if (ord == "Linf")
+	{
 		for (int i = 0; i < vec.n_; ++i)
 			if (fabs(vec[i]) > result)
 				result = fabs(vec[i]);
 		return result;
 	}
-	else {
+	else
+	{
 		throw("Only L2, L1, Linf ord norm possible.");
 	}
 }
 
 template <class T>
-myvector<T>::~myvector() {
+myvector<T>::~myvector()
+{
 	if (v != nullptr)
 		delete[] v;
 }
