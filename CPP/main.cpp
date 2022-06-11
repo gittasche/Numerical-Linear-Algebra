4c1a394e0694e7fc58af15c92e57aa18897c258a
SavkoMax <savcomax@gmail.com>
2022-06-11 23:46:43 +0300

    LU test added

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -6,12 +6,25 @@
 
 int main()
 {
-	initmatdoub init_a = {{4, -1, 1},
-						  {-1, 3, -2},
-						  {1, -2, 3}};
-	matdoub a(3, 3, init_a);
-	Jacobi jac(a, 50);
-	jac.solve();
-	jac.print_res('Y');
+	// Jacobi test
+	// initmatdoub init_a = {{4, -1, 1},
+	// 					  {-1, 3, -2},
+	// 					  {1, -2, 3}};
+	// matdoub a(3, 3, init_a);
+	// Jacobi jac(a, 50);
+	// jac.solve();
+	// jac.print_res('Y');
+
+	// LU test
+	initmatdoub init_a = {{2, 0, 0, 0},
+						  {1, 1.5, 0, 0},
+						  {0, -3, 0.5, 0},
+						  {2, -2, 1, 1}};
+	matdoub a(4, 4, init_a);
+	initvecdoub init_b = {3, 4.5, -6.6, 0.8};
+	vecdoub b(4, init_b);
+	LUdcmp lu(a);
+	vecdoub x = lu.solve(b);
+	std::cout << x;
 	return 0;
 }
